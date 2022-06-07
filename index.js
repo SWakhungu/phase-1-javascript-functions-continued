a228c60cf24a6e092d5ad4d48a00b18c2d1f0c51
Steve Wakhungu <stwakhungu@gmail.com>
2022-06-07 16:45:40 +0300

    functions declared

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+function saturdayFun(funActivity= "roller-skate") {
+    return `This Saturday, I want to ${funActivity}!`
+}
+
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(result="*") {
+    return function wrapAdjective(emphatic="a hard worker") {
+    return `You are ${result}${emphatic}${result}!`
+    }
+}
